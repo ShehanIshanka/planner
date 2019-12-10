d22366a0581a57afdb4b64cbd38cfc98cd5e171a
Shehan Ishanka <ishanka.shehan@gmail.com>
2019-12-10 22:37:06 +0530

    Added missing functionality for project encoder and decoder.

---
+++ lib/model/encoders/projects_json_encoder.dart
@@ -0,0 +1,25 @@
+import 'package:planner/model/beans/project/project.dart';
+import 'package:planner/model/beans/project/projects.dart';
+import 'package:planner/model/encoders/project_json_encoder.dart';
+import 'package:planner/utils/json/json_serde.dart';
+
+class ProjectsJsonEncoder {
+  JsonSerDe _jsonSerDe = new JsonSerDe();
+  ProjectJsonEncoder _projectsJsonEncoder = new ProjectJsonEncoder();
+
+  void encode(Projects projects) {
+    Map<String, List<String>> projectsMap = new Map();
+    projectsEncode(projects, projectsMap);
+    _jsonSerDe.toJson("data", "projects.txt", projectsMap);
+  }
+
+  void projectsEncode(
+      Projects projects, Map<String, List<String>> projectsmap) {
+    List<String> projectFileList = new List();
+    for (Project currentProject in projects.getProjects()) {
+      _projectsJsonEncoder.encode(currentProject);
+      projectFileList.add(currentProject.getFilename());
+    }
+    projectsmap["files"] = projectFileList;
+  }
+}
